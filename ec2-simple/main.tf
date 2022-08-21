df59b8e9387c71e46adae2f0ad661d245d73e1f9
Sahil Suri <sahil.suri@example.com>
2022-08-21 18:52:21 +0000

    working main.tf

--- ec2-simple/main.tf
+++ ec2-simple/main.tf
@@ -13,6 +13,10 @@ provider "aws" {
 }
 
 resource "aws_instance" "web_server" {
- awmi = "ami-090fa75af13c156b4"
+ count = 2
+ ami = "ami-090fa75af13c156b4"
  instance_type = "t2.micro"
+ tags = {
+  Name = "web_server"
+ }
 }
