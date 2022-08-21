8fb426154ca1a8d286fe7bf9b82dd0ce3d6f48e9
Sahil Suri <sahil.suri@example.com>
2022-08-21 18:43:36 +0000

    added .gitignore and configured main.tf

--- ec2-simple/main.tf
+++ ec2-simple/main.tf
@@ -1,1 +1,18 @@
+terraform {
 
+ required_providers {
+  aws = {
+   source = "hashicorp/aws"
+   }
+ }
+
+}
+
+provider "aws" {
+ region = "us-east-1"
+}
+
+resource "aws_instance" "web_server" {
+ awmi = "ami-090fa75af13c156b4"
+ instance_type = "t2.micro"
+}
